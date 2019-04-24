e6b5f62dc74527842e46186334a91829f1e0b25f
Pravin Cherian-Fernandez <c3306899@uon.edu.au>
2019-04-24 05:51:03 +0000

    ####

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include<stdlib.h>
 #include<stdio.h>
-
+#include<string.h>
 void encryptRotation(){
 	
    int num;
    char s[256];
+   
+   //fflush(stdout);
    printf("Enter rotation: ");
    scanf("%d", &num);
-   printf("Enter string: ");
-   //fflush(stdin);
-  //scanf("%s",&s);
+  
+  getchar();
+  printf("Enter string: ");
+ // scanf("%[^\n]s",s);
   gets(s);
-   printf("Value of rotation= %d\n", num);
-   printf("Value of string= %s\n", s);
-
+  printf("Value of rotation= %d\n", num);
+  printf("Value of string= %s\n", s);
    int len = strlen(s);
    int i=0;
    for(i=0;i<len;i++)
@@ -26,17 +36,20 @@ void encryptRotation(){
     	s[i]=65+(s[i]-90)-1;
 	}
    }
-      printf("After Encryption = %s\n", s);
+   
+ printf("After Encryption = %s\n", s);
   
   }
+  
+  
 
 void decryptRotation(){
 int num;
    char s[256];
    printf("Enter Rotation: ");
    scanf("%d", &num);
+   getchar();
    printf("Enter cipher:");
-   fflush(stdin);
    gets(s);
    printf("Value of rotation= %d\n", num);
    printf("Value of string= %s\n", s);
@@ -61,8 +74,9 @@ void encryptSubstitution(){
    char sub[26];
    printf("Enter substitue key: ");
    scanf("%s", &sub);
+   getchar();
    printf("Enter message:");
-   fflush(stdin);
+
    gets(s);
    printf("Value of substitute string= %s\n", sub);
    printf("Value of string= %s\n", s);
@@ -88,8 +102,9 @@ int num;
    
    printf("Enter substitue key: ");
    scanf("%s", &sub);
+   getchar();
    printf("Enter cipher:");
-   fflush(stdin);
+
    gets(s);
 
    int len =strlen(s);
@@ -111,17 +126,19 @@ int num;
    }
       printf("After Decryption = %s\n", s);
   
-}
-void main(){
-	printf("Select any one option: \n");
+}  
+
+int main()
+{	printf("Select any one option: \n");
 	printf(" 1. Encryption of a message with a rotation cipher given the message text and rotation amount \n 2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount\n 3. Encryption of a message with a substitution cipher given message text and alphabet substitution \n 4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions\n");
 	//printf("1 \n2 \n3 \n4 \n5 \n6 \n");
 	int option;
 	scanf("%d",&option);
+
 	if(option==1){
 	encryptRotation();	
 	}
-	if(option==2){
+if(option==2){
 	decryptRotation();	
 	}
 	if(option==3)
@@ -132,4 +149,5 @@ void main(){
 	{
 	 decryptSubstitution();
 	}
+	return 0;
 }
