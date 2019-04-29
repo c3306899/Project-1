86c76cbaa66856e8e5866af8517e85a796d3fed7
Pravin Cherian-Fernandez <c3306899@uon.edu.au>
2019-04-29 04:36:51 +0000

    *This is final Assessment with all of the comments and code.*

--- main.c
+++ main.c
@@ -1,35 +1,42 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+
+//Option 1 Encryption Rotation Cipher
 void encryptRotation(){
 	
    int num;
    char s[256];
    
-   //fflush(stdout);
-   printf("Enter rotation: ");
+   printf("Enter Rotation Amount: ");
    scanf("%d", &num);
   
   getchar();
-  printf("Enter string: ");
- // scanf("%[^\n]s",s);
+  printf("Enter Message For Encryption: ");
+  
   gets(s);
-  printf("Value of rotation= %d\n", num);
-  printf("Value of string= %s\n", s);
+  printf("Value of Rotation= %d\n", num);
+  printf("Message for Encryption= %s\n", s);
+   //Get length of string
    int len = strlen(s);
-   int i=0;
-   for(i=0;i<len;i++)
+   //Loop for string length
+   for(int i=0;i<len;i++)
    {
+    //Check if character is lowercase, then convert to upper if true
    	if(s[i]>=97 && s[i]<=122)
    	s[i]-=32;
+   	//Check if char is in alphabet range
     if(s[i]>=65 && s[i]<=90){
+        //Increment (shift) the character based on the rotation provided
     	s[i]+=num;
+    	//Check is character change goes out of bounds of alphabet
     	if(s[i]>90)
+    	//Loop back to start of the alphabet
     	s[i]=65+(s[i]-90)-1;
 	}
    }
    
- printf("After Encryption = %s\n", s);
+ printf("Encrypted Message = %s\n", s);
   
   }
   
@@ -37,18 +44,18 @@ void encryptRotation(){
 
 void decryptRotation(){
 int num;
+    //Full ASCII Table
    char s[256];
-   printf("Enter Rotation: ");
+   printf("Enter Rotation Amount: ");
    scanf("%d", &num);
    getchar();
-   printf("Enter cipher:");
+   printf("Enter Ciphertext:");
    gets(s);
-   printf("Value of rotation= %d\n", num);
-   printf("Value of string= %s\n", s);
-
+   printf("Value of Rotation= %d\n", num);
+   printf("Decrypted Message= %s\n", s);
+   //Get the length of the string
    int len = strlen(s);
-   int i=0;
-   for(i=0;i<len;i++)
+   for(int i=0;i<len;i++)
    {
     if(s[i]>=65 && s[i]<=90){
     	s[i]-=num;
@@ -56,7 +63,7 @@ int num;
     	s[i]=90-(65-s[i])+1;
 	}
    }
-      printf("After Decryption = %s\n", s);
+      printf("Plaintext = %s\n", s);
    
 }
 void encryptSubstitution(){
@@ -64,22 +71,26 @@ void encryptSubstitution(){
    int num;
    char s[256];
    char sub[26];
-   printf("Enter substitue key: ");
+   //Enter full substitution alphabet
+   printf("Enter substitute key: ");
    scanf("%s", &sub);
    getchar();
-   printf("Enter message:");
+   printf("Enter Message to Encrypt:");
 
    gets(s);
    printf("Value of substitute string= %s\n", sub);
-   printf("Value of string= %s\n", s);
+   printf("Plaintext message= %s\n", s);
 
    int len = strlen(s);
    int i=0;
    for(i=0;i<len;i++)
    {
+     //Convert any lowercase to upper
    	if(s[i]>=97 && s[i]<=122)
    	s[i]-=32;
-    if(s[i]>=65 && s[i]<=90){
+  
+  if(s[i]>=65 && s[i]<=90){
+      //Swap corresponding plaintext with ciphertext from sub key
        s[i]=sub[s[i]-65];
 	}
    }
@@ -110,6 +121,7 @@ int num;
     if(s[i]==sub[j])
     {
     //	printf("%c %d \n",s[i],j);
+    //convert back to ASCII table
 		s[i]=65+j;
 		break;
 	}
@@ -124,6 +136,7 @@ int main()
 {	printf("Select any one option: \n");
 	printf(" 1. Encryption of a message with a rotation cipher given the message text and rotation amount \n 2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount\n 3. Encryption of a message with a substitution cipher given message text and alphabet substitution \n 4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions\n");
 	//printf("1 \n2 \n3 \n4 \n5 \n6 \n");
+	//Printing Options 1-6
 	int option;
 	scanf("%d",&option);
 
@@ -143,3 +156,4 @@ if(option==2){
 	}
 	return 0;
 }
+
